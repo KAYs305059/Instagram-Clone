d53d7d03387976cad4d4fe59f51a9ed5454ca604
Khadar Ahmed Yusuf <Khadar_yo@hotmail.com>
2020-10-24 04:52:33 +0200

    done

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,68 +1,75 @@
-import { Button } from '@material-ui/core'
-import React, { useState } from 'react'
+import { Button } from '@material-ui/core';
+import React, { useState } from 'react';
 import firebase from 'firebase';
 import { db, storage } from './firebase';
+import './ImageUpload.css';
 
-function ImageUpload({username}) {
-    const [image, setImage] = useState(null);
-    const [progress, setProgress] = useState(0);
-    const [caption, setCaption] = useState('');
+function ImageUpload({ username }) {
+  const [image, setImage] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [caption, setCaption] = useState('');
 
-    const handleChange = (e) => {
-        if (e.target.files[0]){
-            setImage(e.target.files[0]);
-        }
+  const handleChange = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
     }
+  };
 
-    const handleUpload = () => {
-     const uploadTask = storage.ref(`images/${image.name}`).put(image);
+  const handleUpload = () => {
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
 
-     uploadTask.on(
-         'state_changed',
-         (snapshot) => {
-             // progress function ...
-             const progress = Math.round (
-                 (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-             );
-             setProgress(progress);
-         },
-         (error) => {
-            // error function ...
-            console.log(error);
-            alert(error.message);
-        },
-        () => {
-            // completed function ...
-            storage
-            .ref('images')
-            .child(image.name)
-            .getDownloadURL()
-            .then(url => {
-                //post image instide db ...
-                db.collection('posts').add({
-                    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-                    caption: caption,
-                    imageUrl: url,
-                    username: username
-                });
-                
-                setProgress(0);
-                setCaption('');
-                setImage(null);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        // progress function ...
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progress);
+      },
+      (error) => {
+        // error function ...
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        // completed function ...
+        storage
+          .ref('images')
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            //post image instide db ...
+            db.collection('posts').add({
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+              caption: caption,
+              imageUrl: url,
+              username: username,
             });
-        }        
-     )
-    }
 
+            setProgress(0);
+            setCaption('');
+            setImage(null);
+          });
+      }
+    );
+  };
 
-    return (
-        <div>
-        <progress value={progress} max='100' />
-        <input type='text' placeholder='Enter a caption...' onChange={event => setCaption(event.target.value)} value={caption} />
-        <input type='file' onChange={handleChange}/>
-        <Button className='imageupload__button' onClick={handleUpload}>Upload</Button>
-        </div>
-    )
+  return (
+    <div className='imageupload'>
+      <progress className='imageupload__progress' value={progress} max='100' />
+      <input
+        type='text'
+        placeholder='Enter a caption...'
+        onChange={(event) => setCaption(event.target.value)}
+        value={caption}
+      />
+      <input type='file' onChange={handleChange} />
+      <Button className='imageupload__button' onClick={handleUpload}>
+        Upload
+      </Button>
+    </div>
+  );
 }
 
-export default ImageUpload
+export default ImageUpload;
--- src/Post.js
+++ src/Post.js
@@ -1,27 +1,93 @@
-import React from 'react'
+import React from 'react';
 import './Post.css';
-import Avatar from '@material-ui/core/Avatar'
+import Avatar from '@material-ui/core/Avatar';
+import { useState, useEffect } from 'react';
+import { db } from './firebase';
+import firebase from 'firebase';
 
+function Post({ postId, user, username, caption, imageUrl }) {
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState([]);
 
-function Post({username, caption, imageUrl}) {
-    return (
-        <div className='post'>
-            <div className="post__header">
-                <Avatar
-                    className='post__avatar'
-                    alt='Khadar'
-                    src='/static/images/avatar/1.jpg'            
-                />
-                <h3>{username}</h3>                
-            </div>
+  useEffect(() => {
+    let unsubscribe;
+    if (postId) {
+      unsubscribe = db
+        .collection('posts')
+        .doc(postId)
+        .collection('comments')
+        .orderBy('timestamp', 'asc')
+        .onSnapshot((snapshot) => {
+          setComments(snapshot.docs.map((doc) => doc.data()));
+        });
+    }
 
-            <img className='post__image' src={imageUrl} alt='' /> 
-            {/*  image a */}
+    return () => {
+      unsubscribe();
+    };
+  }, [postId]);
 
-            <h2 className='post__text'><strong>{username} </strong>{caption}</h2>
-            {/*  username and caption */}
-        </div>
-    )
+  const postComment = (event) => {
+    event.preventDefault();
+
+    db.collection('posts').doc(postId).collection('comments').add({
+      text: comment,
+      username: user.displayName,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setComment('');
+  };
+
+  return (
+    <div className='post'>
+      <div className='post__header'>
+        <Avatar
+          className='post__avatar'
+          alt='Khadar'
+          src='/static/images/avatar/1.jpg'
+        />
+        <h3>{username}</h3>
+      </div>
+
+      <img className='post__image' src={imageUrl} alt='' />
+      {/*  image a */}
+
+      <h2 className='post__text'>
+        {' '}
+        <strong>{username} </strong>
+        {caption}{' '}
+      </h2>
+      {/*  username and caption */}
+
+      <div className='post__commnets'>
+        {comments.map((comment) => (
+          <p>
+            <strong>{comment.username}</strong> {comment.text}
+          </p>
+        ))}
+      </div>
+
+      {user && (
+        <form className='post__commentBox'>
+          <input
+            className='post__input'
+            type='text'
+            placeholder='Write a comment...'
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <button
+            className='post__button'
+            disabled={!comment}
+            type='submit'
+            onClick={postComment}
+          >
+            Post
+          </button>
+        </form>
+      )}
+    </div>
+  );
 }
 
-export default Post
+export default Post;
